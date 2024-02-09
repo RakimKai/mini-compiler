a64cdddf2f2faab5704788c60c6b3a133b3a8c6c
Ahmed Elshiekh <ahmed.elshiekh@edu.fit.ba>
2024-02-09 17:56:42 +0100

    Initial commit

---
+++ kimbo.py
@@ -0,0 +1,22 @@
+from lexer import *
+from emit import *
+from parse import *
+import sys
+
+def main():
+    print("Smallest compiler ever")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
+
+    lexer = Lexer(source)
+    emitter = Emitter("out.c++")
+    parser = Parser(lexer, emitter)
+
+    parser.program()
+    emitter.writeFile()
+    print("Compiling completed.")
+
+main()
